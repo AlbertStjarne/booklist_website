5ae9e126a5ce0278cdf20a1220b3db8c6e6d6591
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-22 15:52:55 +0100

    adds delete functionality

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+//Delete book
+UI.prototype.deleteBook = function(target) {
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -52,7 +59,7 @@ UI.prototype.clearFields = function() {
   document.getElementById('isbn').value = '';
 }
 
-// Event listeners
+// Event listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
   // Get values from form
   const title = document.getElementById('title').value,
@@ -83,4 +90,20 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 
   e.preventDefault();
 });
+
+// Event listener for delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Shwo messsage
+  ui.showAlert('Book removed!', 'success');
+
+  e.preventDefault
+});
+
 
